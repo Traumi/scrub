1ca93fc9879d105125cb808547dcacae67beca8a
Traumi <brsjdtendo@gmail.com>
2017-11-14 14:29:43 +0100

    Add files via upload

--- C/C_Vitrine.php
+++ C/C_Vitrine.php
@@ -2,8 +2,21 @@
 
 function afficher(){
   if(isset($_SESSION['profil'])){
-    require("M/M_Produit.php");
+    require_once("M/M_Produit.php");
+    require_once("M/M_Image.php");
     $produits = getProduits();
+    require("V/header.html");
+    require("V/vitrine.html");
+    require("V/footer.html");
+  }
+}
+
+function search(){
+  if(isset($_SESSION['profil'])){
+    isset($_POST['i'])?$s = $_POST['i']:$s="";
+    require_once("M/M_Produit.php");
+    require_once("M/M_Image.php");
+    $produits = getProduitByDesignation($s);
     require("V/header.html");
     require("V/vitrine.html");
     require("V/footer.html");
